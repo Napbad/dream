768feced9fba52bfe6517fe577c4e1489dab7787
napbad <napbad.sen@gmail.com>
2024-10-02 10:30:31 +0800

    basic class define

---
+++ src/compiler/gen/ClassCodeGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by napbadsen on 24-10-2.
+//
+
+#include "ClassCodeGenerator.h"
---
+++ src/compiler/gen/ClassCodeGenerator.h
@@ -0,0 +1,16 @@
+//
+// Created by napbadsen on 24-10-2.
+//
+
+#ifndef CLASSCODEGENERATOR_H
+#define CLASSCODEGENERATOR_H
+
+
+
+class ClassCodeGenerator {
+
+};
+
+
+
+#endif //CLASSCODEGENERATOR_H
